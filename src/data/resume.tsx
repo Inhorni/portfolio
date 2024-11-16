5152cdc509a3e697743a950f35abb61a2753f91c
Nick <117300320+Inhorni@users.noreply.github.com>
2024-11-16 07:12:27 +0100

    Portfolio

---
+++ src/data/resume.tsx
@@ -0,0 +1,197 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Nick Stettnisch",
+  initials: "NS",
+  url: "http://localhost:3000",
+  location: "Anklam, MV",
+  locationLink: "",
+  description:
+    "Berufseinsteigender Anwendungsentwickler mit Fokus auf Web-Entwicklung. Und hobbymäßiger Sysyemintegrator",
+  summary:
+    "Schulische Ausbildung beendet am 1.09.2024 | Momentan auf Job suche",
+  avatarUrl: "/me.png",
+  skills: [
+    "React",
+    "Next.js",
+    "HTML",
+    "CSS",
+    "MySQL"
+  ],
+  navbar: [
+  ],
+  contact: {
+    email: "nickstettnisch@gmail.com",
+    tel: "+49 1622472872",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/Inhorni",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://de.linkedin.com/in/nick-stettnisch-50794128b",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://dub.sh/dillion-twitter",
+        icon: Icons.x,
+
+        navbar: false,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "",
+        icon: Icons.youtube,
+        navbar: false,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "GSWE GmbH",
+      href: "https://gswe.de/",
+      badges: [],
+      location: "Remote",
+      title: "Fullstack Entwickler",
+      logoUrl: "/gswe.jfif",
+      start: "Juli 2023",
+      end: "August 2023",
+      description:
+        "Fokus auf Webentwicklung mit Symphony(PHP) und React. Entwicklung von neuen Webanwendungs Funktionen für Kunden.",
+    },
+
+  ],
+  education: [
+    {
+      school: "Wirtschafts Akademie Nord greifswald",
+      href: "https://www.wa-nord.de",
+      degree: "Technischer Assistent",
+      logoUrl: "/wanord.png",
+      start: "2022",
+      end: "2024",
+    },
+    {
+      school: "Regionale Schule Käthe Kollwitz Anklam",
+      href: "https://www.kollwitzschule-anklam.de",
+      degree: "Mittlere Reife",
+      logoUrl: "/kwz.png",
+      start: "2016",
+      end: "2022",
+    },
+  ],
+  projects: [
+    {
+      title: "Schmiede Blecken (Schulprojekt)",
+      href: "https://github.com/Inhorni/SchmiedeBlecken",
+      dates: "Oktober 2023 - November 2023",
+      active: true,
+      description:
+        "Entwicklung einer einfachen, aber effektiven Webanwendung für eine traditionelle Schmiede, die bisher keine Online-Präsenz hatte. Ziel war es, ihre handwerkliche Kunst digital erlebbar zu machen und neue Kunden anzusprechen.",
+      technologies: [
+        "Bootstrap",
+        "HTML",
+        "CSS",
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/Inhorni/SchmiedeBlecken",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/proj2.png",
+      video:
+        "",
+    },
+    {
+      title: "WeAreNord",
+      href: "https://we-are-nord.de/",
+      dates: "Januar 2024 - Februar 2024",
+      active: true,
+      description:
+        "Entwicklung einer Schüler-Website, die kreative Projekte und Produkte der Schüler präsentiert und zum Verkauf anbietet.",
+      technologies: [
+        "Angular.js",
+        "Shopware",
+        "Linux Server",
+        "HTML",
+        "CSS",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://we-are-nord.de/",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/project1.png",
+      video: "",
+    },
+    {
+      title: "Pestalotzi Schule Dermin",
+      href: "https://github.com/Armooooon/Schulwebsite",
+      dates: "Juni 2024 - August 2024",
+      active: true,
+      description:
+        "Klassenprojekt zur Erstellung einer modernen Website für eine Schule in Demmin, um Informationen übersichtlich darzustellen und die Schulgemeinschaft online zu stärken.",
+      technologies: [
+        "Bootstrap",
+        "HTML",
+        "CSS",
+        "PHP",
+        "MySQL",
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/Armooooon/Schulwebsite",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "/proj4.png",
+      video: "",
+    },
+    {
+      title: "ED-EL Fleisch",
+      href: "",
+      dates: "September 2024 - Jetzt",
+      active: true,
+      description:
+        "Entwicklung einer ansprechenden und modernen Homepage für einen echten Kunden für eine Großschlachterei nähe Kölns. Ziel war es, Qualität, Tradition und Transparenz digital widerzuspiegeln und Kunden sowie Partner gezielt anzusprechen.",
+      technologies: [
+        "Next.js",
+        "React.js",
+        "Javascript",
+        "TailwindCSS",
+        "HTML",
+        "CSS",
+      ],
+      links: [
+        {
+          type: "WIP",
+          href: "",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/proje3.png",
+      video:
+        "",
+    },
+  ],
+} as const;
